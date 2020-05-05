6bb64fee0321bdd8d6af43a9ba5091f15f9696fc
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-05 16:56:44 -0500

    Initialized my app with create react appp

---
+++ newest-movies/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "../src/Components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header user="Saul Goodman"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ newest-movies/src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = (props) => {
+    return (
+        <h1>Hello {props.user}, welcome. 😍</h1>
+    );
+};
+
+export default Header;
