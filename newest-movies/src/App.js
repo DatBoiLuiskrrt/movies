8412e72bb76bb3ff0fa77fc2319a379a6d0170a9
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-05 19:00:23 -0500

    Added three bottons that have a onclick callback each

--- newest-movies/src/App.js
+++ newest-movies/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import Header from "../src/Components/Header";
+import React, { useState } from 'react';
+
+
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <Header user="Saul Goodman"/>
+      <button onClick={() => setCount(count + 1)}>Click Me 😜</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <p>You have clicked {count} times</p>
+      <button onClick={() => setCount(count * 2)}> x2 clicks</button>
+      
     </div>
   );
 }
--- newest-movies/src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Header = (props) => {
-    return (
-        <h1>Hello {props.user}, welcome. 😍</h1>
-    );
-};
-
-export default Header;
